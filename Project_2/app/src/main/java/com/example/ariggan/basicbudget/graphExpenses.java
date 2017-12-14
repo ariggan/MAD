776ff48924b91ef0f7d0e26b7b3b50df6cfd3fa8
ariggan <ariel.riggan@colorado.edu>
2017-12-14 09:54:01 -0700

    mip map vals and landscape layout

--- Project_2/app/src/main/java/com/example/ariggan/basicbudget/graphExpenses.java
+++ Project_2/app/src/main/java/com/example/ariggan/basicbudget/graphExpenses.java
@@ -2,12 +2,20 @@ package com.example.ariggan.basicbudget;
 
 import android.content.Intent;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import com.jjoe64.graphview.DefaultLabelFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
 
 public class graphExpenses extends AppCompatActivity {
     double housingCost;
@@ -42,18 +50,31 @@ public class graphExpenses extends AppCompatActivity {
         double percentSocial = (socialCost/max);
         double percentMisc = (miscCost/max);
 
-//        SurfaceView graphSpace = (SurfaceView) findViewById(R.id.graphSpace);
-//        Log.d("Error checking", "1");
-//        SurfaceHolder myHolder = graphSpace.getHolder();
-//        Log.d("Error checking", "2");
-//        Canvas drawSpace = myHolder.lockCanvas();
-//        Log.d("Error checking", "3");
-//        //graphSpace.draw(drawSpace);
-//        Log.d("Error checking", "4");
-//        Paint barColor = new Paint();
-//        barColor.setColor(getResources().getColor(R.color.green));
-//        //drawSpace is apparently Null
-//        drawSpace.drawRect(100,100,1,1,barColor);
-//        myHolder.unlockCanvasAndPost(drawSpace);
+
+        //For graphing elements and how to use them I used this site http://www.android-graphview.org/simple-graph/
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(0,0),
+                new DataPoint(5, housingCost),
+                new DataPoint(10, carCost),
+                new DataPoint(15, socialCost),
+                new DataPoint(20, groceryCost),
+                new DataPoint(25, miscCost),
+                new DataPoint(30,0)
+        });
+        graph.addSeries(series);
+
+// styling
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[] {"","Housing", "Car", "Social", "Grocery","Misc.",""});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+
+        series.setSpacing(40);
+
+// draw values on top
+        series.setDrawValuesOnTop(true);
+        series.setValuesOnTopColor(Color.RED);
+//series.setValuesOnTopSize(50);
     }
 }
